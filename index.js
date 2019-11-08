0361013eee2285874b305ed8e3edacb4dbabde91
LavanyaJay <lavanyajay75@gmail.com>
2019-11-08 08:39:04 +0100

    Adding image table

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+
+const db = require("./db");
+
+const Image = require("./Image/model");
+
 app.listen(port, () => {
 	console.log("Listening on port", +port);
 });
